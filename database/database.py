217924d5118ec19333578123a33a0c70599a9379
trace <yoyort21@protonmail.com>
2023-02-08 15:02:21 +0000

    api update flags + challenges

--- database/database.py
+++ database/database.py
@@ -1,19 +1,18 @@
 from pymongo import MongoClient
-from utils import response, error_response
+from utils import error_response, response
 
 client = MongoClient("mongodb+srv://trace:Sb2O2k3w01aL0CGC@cluster1.gorvlno.mongodb.net/?retryWrites=true&w=majority")
 db = client['mydb']
 teams = db['teams']
 flags = db['flags']
+challenges = db['challenges']
 
-
-def teams_helper(team) -> dict:
+def teams_helper(team):
     return {
         "id": str(team["_id"]),
         "team_name": team["team_name"],
         "score": team["score"],
     }
-
 
 def flags_helper(flag):
     return {
@@ -22,6 +21,14 @@ def flags_helper(flag):
         "flag": flag["flag"],
     }
 
+def challenge_helper(challenge):
+    return {
+        "id": str(challenge["_id"]),
+        "challenge_name": challenge["challenge_name"],
+        "description": challenge["description"],
+        "score": challenge["score"],
+        "file_link": challenge["file_links"],
+    }
 
 def create_team(team_name: str, score: int, security_token):
     data = {
@@ -29,6 +36,7 @@ def create_team(team_name: str, score: int, security_token):
         'score': score
     }
     existing_team = teams.find_one({'team_name': team_name})
+
     if existing_team:
         error_response('failed', f'Team {team_name} already exists')
 
@@ -37,19 +45,8 @@ def create_team(team_name: str, score: int, security_token):
     return response(teams_helper(team), f'Team {team_name} created')
 
 
-def update_team_score(team_name, score, security_token):
-    result = teams.update_one({'team_name': team_name}, {'$set': {'score': score}})
-
-    if result.matched_count == 0:
-        return error_response(['failed'], f'Team: {team_name} not found')
-
-    team = teams.find_one({'team_name': team_name})
-    return response(teams_helper(team), f'Team {team_name} updated')
-
-
 def get_teams():
     result = []
-
     for team in teams.find():
         result.append(teams_helper(team))
     return result
@@ -61,8 +58,15 @@ def delete_team(team_name, security_token):
     if existing_team:
         teams.delete_one({'team_name': team_name})
         return response(None, f'{team_name} deleted.')
-
     return error_response('failed', f'{team_name} not exists')
+
+def update_team_score(team_name, score, security_token):
+    result = teams.update_one({'team_name': team_name}, {'$set': {'score': score}})
+
+    if result.matched_count == 0:
+        return error_response(['failed'], f'Team: {team_name} not found')
+    team = teams.find_one({'team_name': team_name})
+    return response(teams_helper(team), f'Team {team_name} updated')
 
 
 def submit_flag(team_name: str, flag: str, security_token: str = 'hash'):
@@ -74,13 +78,12 @@ def submit_flag(team_name: str, flag: str, security_token: str = 'hash'):
     return error_response('failed', f'Team {team_name} not found')
 
 
-def create_flag(challenge_name: str, security_token):
+def create_flag(challenge_name: str, security_token: str = 'hash'):
     flag = '123123'
     data = {
         'challenge': challenge_name, 
         'flag': flag
     }
-
     existing_challenge = flags.find_one({'challenge': challenge_name})
 
     if existing_challenge:
@@ -89,3 +92,25 @@ def create_flag(challenge_name: str, security_token):
     result = flags.insert_one(data)
     flag = flags.find_one({'_id': result.inserted_id})
     return response(flags_helper(flag), f'Challenge {challenge_name} created')
+
+def create_challenge(challenge_name: str, description: str, score: int, file_links: list[str] = []):
+    data = {
+        'challenge_name': challenge_name,
+        'description': description,
+        'score': score,
+        'file_links': file_links
+    }
+
+    existing_challenge = challenges.find_one({'challenge_name': challenge_name})
+    if existing_challenge:
+        return error_response('failed', f'Challenge {challenge_name} already exists')
+
+    result = challenges.insert_one(data)
+    challenge = challenges.find_one({'_id': result.inserted_id})
+    return response(challenge_helper(challenge), f'Challenge {challenge_name} created')
+
+def get_challenges():
+    data = []
+    for challenge in challenges.find():
+        data.append(challenge_helper(challenge))
+    return response(data, 'success')
--- teams.py
+++ teams.py
@@ -1,36 +1,44 @@
-from fastapi import APIRouter
-from database import create_team, get_teams, update_team_score, delete_team, submit_flag, create_flag
+from fastapi import APIRouter, Query
+from database import database
 
 router = APIRouter(
     prefix="/api",
 )
 
-
 @router.get('/teams')
 async def teams_get():
-    return get_teams()
+    return database.get_teams()
 
 
 @router.post('/teams')
 async def team_create(team_name: str, score: int = 0, security_token: str = 'hash'):
-    return create_team(team_name, score, security_token)
+    return database.create_team(team_name, score, security_token)
 
 
 @router.put('/teams')
 async def team_score_update(team_name: str, score: int, security_token: str = 'hash'):
-    return update_team_score(team_name, score, security_token)
+    return database.update_team_score(team_name, score, security_token)
 
 
 @router.delete('/teams')
 async def team_delete(team_name: str, security_token: str = 'hash'):
-    return delete_team(team_name, security_token)
+    return database.delete_team(team_name, security_token)
 
 
 @router.post('/create_flag')
 async def flag_create(challenge_name: str, security_token: str = 'hash'):
-    return create_flag(challenge_name, security_token)
+    return database.create_flag(challenge_name, security_token)
 
 
 @router.post('/submit_flag')
 async def submit(team_name: str, flag: str, security_token: str = 'hash'):
-    return submit_flag(team_name, flag, security_token)
+    return database.submit_flag(team_name, flag, security_token)
+
+
+@router.post('/challenges')
+async def challenge_create(challenge_name: str, description: str, score: int, file_links: list[str] = Query(default=[])):
+    return database.create_challenge(challenge_name, description, score, file_links)
+
+@router.get('/challenge')
+async def challenge_get():
+    return database.get_challenges()
